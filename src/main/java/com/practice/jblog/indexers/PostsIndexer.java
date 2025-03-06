dfebaf7ebe5adfdbc9d769135c66b0b63b32c407
☆♫~Anastasiia Yermolik~♫☆ <vishnu96vishnu@gmail.com>
2025-03-06 21:39:21 +0100

    Optional check with lambda

--- src/main/java/com/practice/jblog/indexers/PostsIndexer.java
+++ src/main/java/com/practice/jblog/indexers/PostsIndexer.java
@@ -1,14 +1,15 @@
 package com.practice.jblog.indexers;
 
+import com.practice.jblog.adapters.ElasticAdapter;
+import com.practice.jblog.config.IndexPrefix;
 import com.practice.jblog.entity.Post;
 import com.practice.jblog.entity.PostAttributeDefinition;
 import com.practice.jblog.repository.PostRepository;
-import com.practice.jblog.config.IndexPrefix;
-import com.practice.jblog.adapters.ElasticAdapter;
 import com.practice.jblog.service.PostAttributesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -59,9 +60,8 @@ public class PostsIndexer implements Indexer {
     @Transactional
     public void reindexOne(Long postId) throws IOException {
         Optional<Post> post = postRepository.findById(postId);
-        if (!post.isPresent()) {
-            throw new IllegalArgumentException("Post with id " + postId + " not found");
-        }
+
+        post.orElseThrow(() -> new IllegalArgumentException("Post with id " + postId + " not found"));
         elasticAdapter.reindexOne(getIndexName(), post.get());
     }
 
